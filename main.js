bc074abfc188d5949849e1b0db39ab4c4e913fdc
Dean Gjedde <dean.gjedde@gmail.com>
2018-06-28 16:39:59 -0700

    more openshif work

--- main.js
+++ main.js
@@ -69,5 +69,7 @@ fs.readFile('Resume-Current.md', 'utf8', (err, data) => {
   app.get('/', (req, res) => {
     res.send(BoilerPlateHtml(Myhtml));
   });
-  app.listen(8080);
+  const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+  //const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+  app.listen(port);
 });
