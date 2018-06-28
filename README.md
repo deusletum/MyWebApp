d75ed9addfc062500de59047b13468b8a4812e34
Dean Gjedde <dean.gjedde@gmail.com>
2018-06-28 13:40:51 -0700

    Getting basic web app working

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # MyWebApp
 
-My first Node JS app!
+## My first Node JS app
 
-Currently working on express and ES6
+## It's a little sandbox app for learning Modern JavaScript
+
+Learning Road Map
+. Get VS code up and node js install and configured - Done
+. Get ES6 working in node js - Done
+    . Found out I needed babel
+    . Also got eslint working
+. Get a basic web app working with Express - Done
--- main.js
+++ main.js
@@ -2,16 +2,36 @@ import express from 'express';
 import fs from 'fs';
 import showdown from 'showdown';
 
-fs.readFile('Resume-Current.md', 'utf8', (err,data) => {
+function BoilerPlateHtml (myhtml) {
+  const boilerplate = `
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml">
+  <html>
+      <head>
+          <rel="stylesheet" type="text/css" href="css/main.css" />
+          <meta http-equiv="content-type" content="text/php; charset=utf-8" />
+          <title>My Site Template - Welcome!</title>
+      </head>
+      <body>
+        ${myhtml}
+      </body>
+  </html>
+        `
+  console.log(myhtml)
+  return boilerplate;
+}
+
+fs.readFile('Resume-Current.md', 'utf8', (err, data) => {
   if (err) {
     return console.log(err);
   }
   const converter = new showdown.Converter();
-  const html = converter.makeHtml(data);
+  const Myhtml = converter.makeHtml(data);
+  console.log(data);
   const app = express();
   app.get('/', (req, res) => {
-    res.send(html);
-    console.log(html)
+    res.send(BoilerPlateHtml(Myhtml));
   });
   app.listen(3000);
 });
